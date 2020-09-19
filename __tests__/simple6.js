48a22e930a6260907e3c4764249bd508febfe2bc
William Wong <compulim@hotmail.com>
2020-09-19 06:34:10 -0700

    Clean up

--- __tests__/simple6.js
+++ __tests__/simple6.js
@@ -4,6 +4,8 @@
 
 const { decode } = require('base64-arraybuffer');
 const { promisify } = require('util');
+const { resolve } = require('path');
+const { tmpdir } = require('os');
 const fs = require('fs');
 
 const writeFile = promisify(fs.writeFile);
@@ -11,6 +13,6 @@ const writeFile = promisify(fs.writeFile);
 test(`should work 6`, async () => {
   await global.acquireWebDriver(async ({ driver }) => {
     await driver.get('https://example.com/');
-    await writeFile(`./__tests__/__temp__/simple6.png`, new Uint8Array(decode(await driver.takeScreenshot())));
+    await writeFile(resolve(tmpdir(), 'simple6.png'), new Uint8Array(decode(await driver.takeScreenshot())));
   });
 });
